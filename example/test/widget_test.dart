a5bf7de3db5a7176fb2731749f4fb36b3e9474ee
Chihiro <1980705744@qq.com>
2022-11-08 21:31:42 +0800

    测试类

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -11,17 +11,4 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_alipay_hk_example/main.dart';
 
 void main() {
-  testWidgets('Verify Platform version', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
-
-    // Verify that platform version is retrieved.
-    expect(
-      find.byWidgetPredicate(
-        (Widget widget) => widget is Text &&
-                           widget.data!.startsWith('Running on:'),
-      ),
-      findsOneWidget,
-    );
-  });
 }
--- test/flutter_alipay_hk_test.dart
+++ test/flutter_alipay_hk_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_alipay_hk/alipay_result.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_alipay_hk/flutter_alipay_hk.dart';
 import 'package:flutter_alipay_hk/flutter_alipay_hk_platform_interface.dart';
@@ -7,13 +8,23 @@ import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 class MockFlutterAlipayHkPlatform
     with MockPlatformInterfaceMixin
     implements FlutterAlipayHkPlatform {
-
   @override
   Future<String?> getPlatformVersion() => Future.value('42');
+
+  @override
+  Future<bool> isInstalled() => Future.value(true);
+
+  @override
+  Future<AlipayResult> pay(String payInfo) => Future.value(
+      AlipayResult(memo: "11", result: "404", resultStatus: "支付失败"));
+
+  @override
+  Future setIosUrlSchema(String urlSchema) =>Future.value();
 }
 
 void main() {
-  final FlutterAlipayHkPlatform initialPlatform = FlutterAlipayHkPlatform.instance;
+  final FlutterAlipayHkPlatform initialPlatform =
+      FlutterAlipayHkPlatform.instance;
 
   test('$MethodChannelFlutterAlipayHk is the default instance', () {
     expect(initialPlatform, isInstanceOf<MethodChannelFlutterAlipayHk>());
@@ -25,5 +36,7 @@ void main() {
     FlutterAlipayHkPlatform.instance = fakePlatform;
 
     expect(await flutterAlipayHkPlugin.getPlatformVersion(), '42');
+    expect(await flutterAlipayHkPlugin.pay("payInfo"), AlipayResult(memo: "11", result: "404", resultStatus: "支付失败"));
+    expect(await flutterAlipayHkPlugin.isInstalled(), true);
   });
 }
